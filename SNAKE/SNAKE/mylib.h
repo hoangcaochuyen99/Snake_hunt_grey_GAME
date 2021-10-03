b27ff06a1a9d6eba4f937a9922848fd14b46bd1f
hoangcaochuyen99 <74084012+hoangcaochuyen99@users.noreply.github.com>
2021-10-03 21:40:33 +0700

    Update mylib.h

--- SNAKE/SNAKE/mylib.h
+++ SNAKE/SNAKE/mylib.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include<ctime> /* thư viện hỗ trợ về thời gian thực */
-#include "windows.h" // thư viện này bá đạo lắm nhé - chứa nhiều đồ chơi nek - cứ tìm hiểu dần dần s
+#include "windows.h" 
 //======= lấy tọa độ x của con trỏ hiện tại =============
 #define KEY_NONE	-1
 int whereX()
